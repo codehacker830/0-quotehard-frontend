382b4a9bede982effa20fe1903a4589cad480d19
classy-dev <gwolf.bond97@gmail.com>
2020-12-02 04:26:49 +0200

    🐛 FIX: Fix TeamSetting badge number.

--- src/app/routes/Settings/components/AddTeamMember.jsx
+++ src/app/routes/Settings/components/AddTeamMember.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 
 export const AddTeamMember = (props) => {
+   const { teamMembers } = props;
    return (
-      <>
-         <h3 className="settings-title">Team Members <span className="badge badge-info">1</span></h3>
+      <React.Fragment>
+         <h3 className="settings-title">Team Members <span className="badge badge-info">{teamMembers.length}</span></h3>
          <Link className="set-option" to="/app/settings/team">Add a Team Member</Link>
-      </>
+      </React.Fragment>
    )
 }
-
-export default AddTeamMember
+const mapStateToProps = ({ teamSetting }) => ({ teamMembers: teamSetting.teamMembers });
+export default connect(mapStateToProps)(AddTeamMember);
