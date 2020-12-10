41617878e9e311672b05cdcfadf6284cdf073480
classy-dev <gwolf.bond97@gmail.com>
2020-12-11 01:03:16 +0800

    👌 IMPROVE: Team setting routes.

--- src/app/routes/Settings/index.jsx
+++ src/app/routes/Settings/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import { getTeamMembers } from '../../../actions/Team';
+import Team from './Team';
 import asyncComponent from '../../../util/asyncComponent';
 
 class Settings extends Component {
@@ -23,10 +24,7 @@ class Settings extends Component {
             <Route exact path="/app/settings/customer-emails" component={asyncComponent(() => import("./CustomerEmails"))} />
             <Route exact path="/app/settings/customer-email-change/:id" component={asyncComponent(() => import("./CustomerEmailChange"))} />
             <Route exact path="/app/settings/sales-tax-categories" component={asyncComponent(() => import("./SalesTaxCategories"))} />
-            <Route exact path="/app/settings/team" component={asyncComponent(() => import("./Team"))} />
-            <Route exact path="/app/settings/team/invite-form" component={asyncComponent(() => import("./Team/InviteForm"))} />
-            <Route exact path="/app/settings/team/view/:id" component={asyncComponent(() => import("./Team/MemberView"))} />
-
+            <Route path="/app/settings/team" component={Team} />
 
             <Route exact path="/app/settings/billing-overview" component={asyncComponent(() => import("./BillingOverview"))} />
             <Route exact path="/app/settings/account-information" component={asyncComponent(() => import("./AccountInformation"))} />
