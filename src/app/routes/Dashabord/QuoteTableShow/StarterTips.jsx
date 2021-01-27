2acaa5aa48b10ebb902e64d1fc9b98c04483b79a
codemagician45 <gwolf.bond97@gmail.com>
2021-01-27 19:36:37 +0800

    👌 IMPROVE: Getting started checklist

--- src/app/routes/Dashabord/QuoteTableShow/StarterTips.jsx
+++ src/app/routes/Dashabord/QuoteTableShow/StarterTips.jsx
@@ -1,31 +1,62 @@
+import clsx from 'clsx'
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export default class StarterTips extends Component {
-    render() {
-        return (
-            <div className="starterTips">
-                <h3 className="starterTips-title">Getting started checklist</h3>
-                <ul>
-                    <li className=" starterTips-done">
-                        <i className="fa fa-fw fa-check" /> Quotehard account created </li>
-                    <li className=" starterTips-done">
-                        <i className="fa fa-fw fa-check" /> Company logo. Nice! </li>
-                    <li className="">
-                        <i className="far fa-fw fa-square" /> First quote sent </li>
-                    <li className="">
-                        <i className="far fa-fw fa-square" /> First quote accepted </li>
-                    <li className="">
-                        <Link to="/app/settings"><i className="far fa-fw fa-square" /> Enable an Add-on or App…</Link>
-                    </li>
-                    <li className="">
-                        <Link to="/app/settings/team"><i className="far fa-fw fa-square" /> Invite a Team Member…</Link>
-                    </li>
-                </ul>
-                <p>
-                    <button className="btn btn-default btn-sm">Hide Checklist</button>
-                </p>
-            </div>
-        )
+export default function StarterTips() {
+    const isAddLogoDone = true;
+    const isFirstQuoteSent = true;
+    const isFirstQuoteAccepted = true;
+    const isAddOnEnabled = true;
+    const isTeamMemberInvited = true;
+    const onHandleClick = () => {
+
     }
+    return (
+        <div className="starterTips">
+            <h4 className="starterTips-title">Getting started checklist</h4>
+            <ul>
+                <li className=" starterTips-done">
+                    <i className="fa fa-fw fa-check" /> Quotehard account created </li>
+                {
+                    isAddLogoDone ?
+                        <li className=" starterTips-done">
+                            <i className="fa fa-fw fa-check" /> Company logo. Nice!</li>
+                        : <li className="">
+                            <Link to="/app/settings/quote/appearance"><i className="far fa-fw fa-square" /> Add logo</Link></li>
+                }
+                {
+                    isFirstQuoteSent ?
+                        <li className=" starterTips-done">
+                            <i className="fa fa-fw fa-check" /> Quote sent </li>
+                        : <li className="">
+                            <i className="far fa-fw fa-square" /> First quote sent </li>
+                }
+                {
+                    isFirstQuoteAccepted ?
+                        <li className=" starterTips-done">
+                            <i className="fa fa-fw fa-check" /> Quote accepted </li>
+                        : <li className="">
+                            <i className="far fa-fw fa-square" /> First quote accepted </li>
+                }
+                {
+                    isAddOnEnabled ?
+                        <li className=" starterTips-done">
+                            <i className="fa fa-fw fa-check" /> Add-on enabled! </li>
+                        : <li className="">
+                            <Link to="/app/settings"><i className="far fa-fw fa-square" /> Enable an Add-on or App…</Link></li>
+                }
+                {
+                    isTeamMemberInvited ?
+                        <li className=" starterTips-done">
+                            <i className="fa fa-fw fa-check" /> Team Member invited! </li>
+                        : <li className="">
+                            <Link to="/app/settings/team"><i className="far fa-fw fa-square" /> Invite a Team Member…</Link></li>
+                }
+            </ul>
+            <p>
+                <button className="btn btn-default btn-sm" onClick={onHandleClick}>Hide Checklist</button>
+            </p>
+        </div >
+    )
 }
+
