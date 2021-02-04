0f0a427f9914ff3b954879e05d21a82ce05586eb
codemagician45 <gwolf.bond97@gmail.com>
2021-02-04 05:38:39 +0200

    🐛 FIX: Show All... button in template settings

--- src/components/TemplateSettings.jsx
+++ src/components/TemplateSettings.jsx
@@ -7,7 +7,7 @@ import TaxMode from './QuoteSettings/TaxMode';
 
 class TemplateSettings extends React.Component {
    state = {
-      show: true
+      show: false
    }
    render() {
       return (
@@ -38,7 +38,7 @@ class TemplateSettings extends React.Component {
                      <TaxMode />
                      <PricingDisplayLevel />
                   </div>
-                  <button type="button" className={`btn btn-sm btn-outline-dark ${this.state.show ? "d-none" : ""}`} onClick={() => this.setState({ show: true })}>Show All...</button>
+                  <button type="button" className={`btn btn-sm btn-default ${this.state.show ? "d-none" : ""}`} onClick={() => this.setState({ show: true })}>Show All...</button>
                </div>
             </div>
          </div>
