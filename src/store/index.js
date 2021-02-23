0222927acdf38e8a86e02cb620ead7a30dd55c40
codemagician45 <gwolf.bond97@gmail.com>
2021-02-23 13:01:26 +0200

    👌 IMPROVE: Stripe live key integration

--- src/store/index.js
+++ src/store/index.js
@@ -9,9 +9,8 @@ const history = createBrowserHistory();
 const routeMiddleware = routerMiddleware(history);
 
 let middlewares = [thunk, routeMiddleware, createLogger()];
-if (process.env.NODE_ENV === 'production') {
-   middlewares = [thunk, routeMiddleware];
-}
+if (process.env.NODE_ENV === 'production') middlewares = [thunk, routeMiddleware];
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
